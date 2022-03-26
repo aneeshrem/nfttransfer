e11b945cdd41830270189154d9d9ca0749c97b6d
Aneesh <aneeshm.rem@gmail.com>
2022-03-26 12:47:11 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -100,44 +100,14 @@ export default function App() {
     return <h2>Could not find a provider</h2>;
   }
   const fromwallet = new Uint8Array([193,47,61,157,108,137,241,249,71,124,31,187,115,154,174,79,250,125,148,26,6,137,84,38,144,25,87,224,58,133,116,157,6,162,23,15,133,42,247,207,234,38,19,135,17,169,74,217,243,221,6,10,22,108,74,177,145,104,232,3,97,31,114,187]);  //address : stone
-  const towallet=new Uint8Array([107,253,16,174,143,141,9,126,209,110,18,43,218,226,85,223,96,45,205,184,32,101,19,227,98,241,52,184,154,135,238,2,187,231,31,133,185,46,31,157,75,250,31,53,38,207,50,40,222,84,175,56,214,127,37,54,86,44,138,194,79,81,106,139])
-
-  let sender;
+  let marketplaceWallet = solanaWeb3.Keypair.fromSecretKey(fromwallet)
   let receiver;
-  const useLocalWallet=false;
-
-  if(useLocalWallet)//use local wallet
-  {
-    sender = solanaWeb3.Keypair.fromSecretKey(fromwallet)
-    receiver = solanaWeb3.Keypair.fromSecretKey(towallet)
-  }
-  else
-  {
-    sender = solanaWeb3.Keypair.fromSecretKey(fromwallet)
-    receiver = provider
-  }
-  
+  receiver = provider  
 
   const sentamount = 0.01
   const recevieamount = 0.02
 
-  const instructions=[];
-
-  instructions.push(new solanaWeb3.Transaction().add(
-    solanaWeb3.SystemProgram.transfer({
-      fromPubkey: sender.publicKey,
-      toPubkey: receiver.publicKey,
-      lamports: sentamount * 1000000000
-    }),
-  )); 
-
-  instructions.push(new solanaWeb3.Transaction().add(
-    solanaWeb3.SystemProgram.transfer({
-      fromPubkey: receiver.publicKey,
-      toPubkey: sender.publicKey,
-      lamports: recevieamount * 1000000000
-    }),
-  ));               
+             
 	
   const getTxSignature = async (signer, instruction1, blockhash) => 
   {
@@ -154,99 +124,78 @@ export default function App() {
       });
       const signature = tx.signatures.find(signature => signature.publicKey.toBase58() === signer.publicKey.toBase58())
       return signature
-  }
-
-  const getTxSignatureLocal= async (signer, instruction1, blockhash) => 
-  {
-      addLog('getTxSignaturelocal in :'+signer.publicKey)
-      const tx = new solanaWeb3.Transaction()
-      tx.add(...instruction1)        
-      tx.recentBlockhash = blockhash
-      tx.feePayer = signer.publicKey     
-      tx.sign(signer)
-      //let signed = await signer.signTransaction(tx);
-      //addLog('signed -: '+signed)
-
-      tx.signatures.forEach((signature) => {
-        addLog("signature local:"+signature.publicKey.toBase58());
-      });
-      const signature = tx.signatures.find(signature => signature.publicKey.toBase58() === signer.publicKey.toBase58())
-      return signature
-  }
-
-  const getTxSignatureWeb= async (signer, instruction1, blockhash) => 
-  {
-      addLog('getTxSignatureWeb in :'+signer.publicKey)
-      const tx = new solanaWeb3.Transaction()
-      tx.add(...instruction1)        
-      tx.recentBlockhash = blockhash
-      tx.feePayer = signer.publicKey     
-     
-      let signed = await signer.signTransaction(tx);
-      addLog('signed -: '+signed)
-
-      tx.signatures.forEach((signature) => {
-        addLog("signature Web:"+signature.publicKey.toBase58());
-      });
-      const signature = tx.signatures.find(signature => signature.publicKey.toBase58() === signer.publicKey.toBase58())
-      return signature
-  }
-
-
-  
+  }  
   
 
   const signMultipleTransactionMultiple = async () => {
     try 
     {    
-      
+     
+      const instructions=[];
+
+      var mintTokenAddress="4UhA7u8zxPYmt1YHgV7MnrxyZ51RDY8W2DWJiwSCtzV3";
+      var mintTokenPub = new solanaWeb3.PublicKey(mintTokenAddress);
+      var mintToken = new splToken.Token(connection,mintTokenPub,splToken.TOKEN_PROGRAM_ID, marketplaceWallet);
+      var marketplaceTokenAccount = await mintToken.getOrCreateAssociatedAccountInfo( marketplaceWallet.publicKey)
+      var toTokenAccount = await mintToken.getOrCreateAssociatedAccountInfo(receiver.publicKey)
+
+      instructions.push(new solanaWeb3.Transaction().add(
+        splToken.Token.createTransferInstruction( 
+            splToken.TOKEN_PROGRAM_ID,  
+            marketplaceTokenAccount.address,                       
+            toTokenAccount.address,
+            marketplaceWallet.publicKey,
+            [],
+            1)
+        )
+    );
+    
+      instructions.push(new solanaWeb3.Transaction().add(
+        solanaWeb3.SystemProgram.transfer({
+          fromPubkey: receiver.publicKey,
+          toPubkey: marketplaceWallet.publicKey,
+          lamports: recevieamount * 1000000000
+        }),
+      ));  
+
+
+
       
       const blockhash = (await connection.getRecentBlockhash()).blockhash     
-      addLog('from account :' + sender.publicKey);
-      addLog('to account :' + receiver.publicKey);
-      var receiverSignature;
+      addLog('from account :' + marketplaceWallet.publicKey);
+      addLog('to account :' + receiver.publicKey);     
 
-      // getting signatures
-   
-      
-      if(useLocalWallet){
-          receiverSignature = await getTxSignatureLocal(receiver, instructions, blockhash)
-      }
-      else{
-        receiverSignature = await getTxSignatureWeb(receiver, instructions, blockhash)
-      }       
-
-      const senderSignature = await getTxSignature(sender, instructions, blockhash)
-
-      addLog('senderSignature :'+senderSignature.signature)
-      addLog('receiverSignature :'+receiverSignature.signature)
+      const senderSignature = await getTxSignature(marketplaceWallet, instructions, blockhash)
 
       const tx = new solanaWeb3.Transaction().add(...instructions)
       tx.recentBlockhash = (await connection.getRecentBlockhash()).blockhash
       tx.feePayer = receiver.publicKey
 
       tx.addSignature(senderSignature.publicKey, senderSignature.signature)
-      tx.addSignature(receiverSignature.publicKey, receiverSignature.signature)
 
-      addLog(`txhash: ${await connection.sendRawTransaction(tx.serialize())}`);
-
-      // let signature1 = await connection.sendRawTransaction(transactiondata1.serialize());
-      // addLog("Submitted transaction sol : " + signature1 + ", awaiting confirmation");
-      // await connection.confirmTransaction(signature1);
-      // addLog("Transaction sol: " + signature1 + " confirmed");       
-      
-      
-      
-      // let signature2 = await connection.sendRawTransaction(transactiondata2.serialize());
-      // addLog("Submitted transaction nft : " + signature2 + ", awaiting confirmation");
-      // await connection.confirmTransaction(signature2);
-      // addLog("Transaction nft : " + signature2 + " confirmed");
+      let signed = await receiver.signTransaction(tx);
+      addLog(`txhash: ${await connection.sendRawTransaction(tx.serialize())}`);  
       
     } catch (err) {
       console.warn(err);
       addLog("[error] signMultipleTransactions: " + JSON.stringify(err));
     }
   };
+
+  
+  const ClearText = async () => {
+    try 
+    {    
+      const myNode = document.getElementById("divLog");
+      myNode.innerHTML = '';      
+      
+      
+    } catch (err) {
+      console.warn(err);
+      addLog("[error] ClearText: " + JSON.stringify(err));
+    }
+  };
+
  
   return (
     <div className="App">
@@ -275,6 +224,9 @@ export default function App() {
             >
               Disconnect
             </button>
+            <button onClick={() => ClearText()}>
+              Clear{" "}
+            </button>
           </>
         ) : (
           <>
@@ -293,7 +245,7 @@ export default function App() {
           </>
         )}
       </main>
-      <footer className="logs">
+      <footer id="divLog" className="logs">
         {logs.map((log, i) => (
           <div className="log" key={i}>
             {log}
